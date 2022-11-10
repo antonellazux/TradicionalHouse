b783bb955b5e4a820544f4ecebc06d2da5e2f8c8
antonellazux <zuniga.antonella.epet20@gmail.com>
2022-11-10 16:22:03 -0300

    confeccionando todas las galerias

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const barr = document.querySelector(".barr");
+const menu = document.querySelector(".menu");
+barr.addEventListener("click", function () {
+    
+}(){
+    menu.classListe.toggle(".mostrar");
+});
+const subMenu =document.querySelectorAll(".subMenu");
+for(let i<0; i < subMenu.length; i++){
+    subMenu[i].addEventListener("click", function () {
+        
+    }{
+        if (window.innerWidth <1024) {
+            
+        }
+    });
+}
